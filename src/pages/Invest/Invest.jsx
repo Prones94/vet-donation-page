5e5c7b76d5b3f6541c37f6f0b50b76fbd0ddd6ed
Michael F Alvarez <michael.fred.alvarez@gmail.com>
2021-12-16 11:58:02 -0800

    update slider and input value, include api call to checkout

--- src/pages/Invest/Invest.jsx
+++ src/pages/Invest/Invest.jsx
@@ -1,10 +1,6 @@
-// material
 import { styled } from '@mui/material/styles';
-import { Grid, Container, Typography } from '@mui/material';
-// components
+import { Grid, Container, Typography, Box, Link } from '@mui/material';
 import { InvestPlanCard } from './InvestPlanCard';
-import { CustomPlanCard } from './CustomPlanCard'
-//
 import { plans } from './plans';
 
 // ----------------------------------------------------------------------
@@ -19,35 +15,38 @@ const RootStyle = styled('div')(({ theme }) => ({
 
 const Invest = () => {
   return (
-    <RootStyle title="Pricing | Minimal-UI">
+    <RootStyle>
       <Container maxWidth="lg">
         <Typography variant="h3" align="center" paragraph>
           Invest plans for your
-          <br /> community&apos;s size and needs
+          <br /> 
+          community's size and needs
         </Typography>
         <Typography align="center" sx={{ color: 'text.secondary' }}>
           Choose your plan and make modern online conversation magic
         </Typography>
 
-        <Grid container spacing={3}>
-          {/* {plans?.map((card, index) => (
+        { (true) && (
+          <Box>
+            <Typography align="center">
+              if you want to invest and checkout,&ensp;
+              <Link>
+                login here
+              </Link>
+            </Typography>
+          </Box>
+        )}
+
+        <Grid container spacing={3} sx={{ mt: 1 }}>
+          { plans?.map((card, index) => (
             <Grid item xs={12} md={4} key={card.payment}>
-              { (index < 2) && <InvestPlanCard card={card} index={index} />}
+              <InvestPlanCard card={card} index={index} />
             </Grid>
-          ))} */}
-          <Grid item xs={12} md={4}>
-            <InvestPlanCard card={plans[0]} index={0} />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <InvestPlanCard card={plans[1]} index={1} />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <CustomPlanCard card={plans[2]} index={2} />
-          </Grid>
+          ))}
         </Grid>
       </Container>
     </RootStyle>
   );
-}
+};
 
 export { Invest }
