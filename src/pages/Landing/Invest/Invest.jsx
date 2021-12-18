10149e8ac6b9895f85f2a3b9d6a4b712144adea3
sanixstudio <sanixstudio@gmail.com>
2021-12-17 16:21:41 -0800

    Change description and title typography and apply color to title and button

--- src/pages/Landing/Invest/Invest.jsx
+++ src/pages/Landing/Invest/Invest.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
-import { Icon } from '@iconify/react';
-import roundArrowRightAlt from '@iconify/icons-ic/round-arrow-right-alt';
-// material
+import { Link as RouterLink } from 'react-router-dom';
 import { alpha, useTheme, styled } from '@mui/material/styles';
 import { Box, Grid, Button, Container, Typography } from '@mui/material';
-// utils
-// import { MHidden } from '../@material-extend';
-// import { varFadeInUp, varFadeInRight, MotionInView } from '../animate';
-
-// ----------------------------------------------------------------------
 
 const RootStyle = styled('div')(({ theme }) => ({
   textAlign: 'center',
@@ -20,7 +13,7 @@ const RootStyle = styled('div')(({ theme }) => ({
 }));
 
 const Invest = () => {
-    const theme = useTheme();
+  const theme = useTheme();
   const isLight = theme.palette.mode === 'light';
   const shadow = `-40px 40px 80px ${alpha(isLight ? theme.palette.grey[500] : theme.palette.common.black, 0.48)}`;
 
@@ -28,11 +21,9 @@ const Invest = () => {
     <RootStyle>
       <Container maxWidth="lg">
         <Grid container spacing={3}>
-          {/* <MHidden width="mdDown"> */}
           <Grid item xs={12} md={6} lg={7} sx={{ pr: { md: 7 } }}>
             <Grid container spacing={3} alignItems="flex-end">
               <Grid item xs={6}>
-                {/* <MotionInView variants={varFadeInUp}> */}
                 <Box
                   component="img"
                   src="/static/about/what-1.jpg"
@@ -41,48 +32,33 @@ const Invest = () => {
                     boxShadow: shadow
                   }}
                 />
-                {/* </MotionInView> */}
               </Grid>
               <Grid item xs={6}>
-                {/* <MotionInView variants={varFadeInUp}> */}
                 <Box component="img" src="/static/about/what-2.jpg" sx={{ borderRadius: 2 }} />
-                {/* </MotionInView> */}
               </Grid>
             </Grid>
           </Grid>
-          {/* </MHidden> */}
-
           <Grid item xs={12} md={6} lg={5}>
-            {/* <MotionInView variants={varFadeInRight}> */}
-            <Typography variant="h2" sx={{ mb: 3 }}>
-              Invest in our product
+            <Typography variant="h2" sx={{ mb: 3, fontWeight: 'bold' }}>
+              <Typography variant="span" sx={{ color: 'info.light' }}>Invest</Typography> in our product
             </Typography>
-            {/* </MotionInView> */}
-
-            {/* <MotionInView variants={varFadeInRight}> */}
             <Typography
               sx={{
+                fontSize: 22,
                 color: (theme) => (theme.palette.mode === 'light' ? 'text.secondary' : 'common.white')
               }}
             >
-              Our theme is the most advanced and user-friendly theme you will find on the market, we have
-              documentation and video to help set your site really easily, pre-installed demos you can import in one
-              click and everything from the theme options to page content can be edited from the front-end. This is
-              the theme you are looking for.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ullam neque laudantium, rem
+              cum dicta. Nesciunt sit ipsam cupiditate voluptatem!
             </Typography>
-            {/* </MotionInView> */}
-
-            {/* <MotionInView variants={varFadeInRight}> */}
             <Button
-              variant="outlined"
-              color="inherit"
+              sx={{ marginY: 6, backgroundColor: 'info.light' }}
+              variant="contained"
               size="large"
-              sx={{ marginTop: 4, fontWeight: 'bold' }}
-              // endIcon={<Icon icon={roundArrowRightAlt} width={24} height={24} />}
-            >
+              component={RouterLink}
+              to={"/"}>
               Invest Now
             </Button>
-            {/* </MotionInView> */}
           </Grid>
         </Grid>
       </Container>
