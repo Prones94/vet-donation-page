04a0047049f2f26644e5b6c56bea6cdfd0150b80
Adnan Niaz <sanixstudio@gmail.com>
2021-12-20 17:31:00 -0800

    Create Navbar, connect donate component to its route, and show Hero section

---
+++ src/containers/Header/Navbar/Navbar.jsx
@@ -0,0 +1,141 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import SyncLockIcon from '@mui/icons-material/SyncLock';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import { Link } from '@mui/material';
+
+// const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  // const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ backgroundColor: 'grey.900' }}>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography
+              variant="h5"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex', fontWeight: 'bold', alignItems: 'center' } }}
+            >
+              <SyncLockIcon sx={{ color: 'primary.light', fontSize: '2em', marginRight: '.5em' }} />
+              Secure Social Media
+            </Typography>
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              <Box sx={{
+                padding: '1em',
+                display: 'flex',
+                flexDirection: 'column',
+                minWidth: '91vw',
+              }}>
+                <Button>
+                  <Link href="/" sx={{ color: 'inherit', textDecoration: 'none' }}>home</Link>
+                </Button>
+                <Button>
+                  <Link href="/" sx={{ color: 'inherit', textDecoration: 'none' }}>donate</Link>
+                </Button>
+                <Button>
+                  <Link href="/" sx={{ color: 'inherit', textDecoration: 'none' }}>invest</Link>
+                </Button>
+                <Button>
+                  <Link href="/" sx={{ color: 'inherit', textDecoration: 'none' }}>contact</Link>
+                </Button>
+                <Button>
+                  <Link href="/" sx={{ color: 'inherit', textDecoration: 'none' }}>Login</Link>
+                </Button>
+                <Button>
+                  <Link href="/" sx={{ color: 'inherit', textDecoration: 'none' }}>Logout</Link>
+                </Button>
+              </Box>
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            <SyncLockIcon sx={{ color: 'primary.light', fontSize: '2em', marginRight: '.5em' }} />
+            Secure Social Media
+          </Typography>
+          <Box sx={{
+            flexGrow: 1,
+            gap: '1em',
+            justifyContent: 'flex-end',
+            marginRight: '1em',
+            display: { xs: 'none', md: 'flex' }
+          }}>
+            <Button>
+              <Link href="/" sx={{ color: 'white', textDecoration: 'none' }}>home</Link>
+            </Button>
+            <Button>
+              <Link href="/donate" sx={{ color: 'white', textDecoration: 'none' }}>donate</Link>
+            </Button>
+            <Button>
+              <Link href="/invest" sx={{ color: 'white', textDecoration: 'none' }}>invest</Link>
+            </Button>
+            <Button>
+              <Link href="/contact" sx={{ color: 'white', textDecoration: 'none' }}>contact</Link>
+            </Button>
+            <Button>
+              <Link href="/login" sx={{ color: 'white', textDecoration: 'none' }}>Login</Link>
+            </Button>
+            <Button>
+              <Link href="/" sx={{ color: 'white', textDecoration: 'none' }}>Logout</Link>
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import { Donate, Invest } from './';
-// import { Donate, Hero, Invest } from './';
+import { Donate, Hero, Invest } from './';
 
 const Landing = () => {
 	return (
 		<>
-			{/* <Hero /> */}
+			<Hero />
 			<Donate />
 			<Invest />
 		</>
